174373c36ec4a106bd9eef71a416f8316e134d73
SaoYan <25550286+SaoYan@users.noreply.github.com>
2023-10-08 10:27:42 -0700

    Create project

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yiqi Yan on 2023-10-08.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
